88b1882c1a10afafe479402ba2696638684572da
Hongjie97 <944685606@qq.com>
2022-11-23 15:52:32 +0800

    Update ConvNet.py

--- ConvNet.py
+++ ConvNet.py
@@ -30,19 +30,19 @@ class DeConvLayer(nn.Module):
 class ConvAutoEncoder(nn.Module):
     def __init__(self, nChannels):
         super(ConvAutoEncoder, self).__init__()
-        # Encoder
+        # encoder
         self.Conv1 = ConvBlock(1, nChannels)
-        self.Conv2 = ConvBlock(nChannels, nChannels * 2)
-        self.Conv3 = ConvBlock(nChannels * 2, nChannels * 4)
-        self.Conv4 = ConvBlock(nChannels * 4, nChannels * 8)
+        self.Conv2 = ConvBlock(nChannels, nChannels*2)
+        self.Conv3 = ConvBlock(nChannels*2, nChannels*4)
+        self.Conv4 = ConvBlock(nChannels*4, nChannels*8)
         self.AvgPool = nn.AvgPool3d(kernel_size=2, stride=2)
 
-        # Decoder
-        self.deConv1 = DeConvLayer(nChannels * 8, nChannels * 4)
-        self.Conv5 = ConvBlock(nChannels * 4, nChannels * 4)
-        self.deConv2 = DeConvLayer(nChannels * 4, nChannels * 2)
-        self.Conv6 = ConvBlock(nChannels * 2, nChannels * 2)
-        self.deConv3 = DeConvLayer(nChannels * 2, nChannels)
+        # decoder
+        self.deConv1 = DeConvLayer(nChannels*8, nChannels*4)
+        self.Conv5 = ConvBlock(nChannels*4, nChannels*4)
+        self.deConv2 = DeConvLayer(nChannels*4, nChannels*2)
+        self.Conv6 = ConvBlock(nChannels*2, nChannels*2)
+        self.deConv3 = DeConvLayer(nChannels*2, nChannels)
         self.Conv7 = ConvBlock(nChannels, nChannels)
         self.Conv8 = nn.Conv3d(nChannels, 1, kernel_size=1)
 
@@ -60,17 +60,17 @@ class ConvAutoEncoder(nn.Module):
 class Feature_Extraction(nn.Module):
     def __init__(self, nChannels):
         super(Feature_Extraction, self).__init__()
-        # Encoder for feature extraction
+        # encoder for feature extraction
         self.Conv1 = ConvBlock(1, nChannels)
-        self.Conv2 = ConvBlock(nChannels, nChannels * 2)
-        self.Conv3 = ConvBlock(nChannels * 2, nChannels * 4)
-        self.Conv4 = ConvBlock(nChannels * 4, nChannels * 8)
-        self.AvgPool = nn.AvgPool3d(kernel_size = 2, stride = 2)
+        self.Conv2 = ConvBlock(nChannels, nChannels*2)
+        self.Conv3 = ConvBlock(nChannels*2, nChannels*4)
+        self.Conv4 = ConvBlock(nChannels*4, nChannels*8)
+        self.AvgPool = nn.AvgPool3d(kernel_size=2, stride=2)
 
     def forward(self, x):
         feature = self.AvgPool(self.Conv1(x))
         feature = self.AvgPool(self.Conv2(feature))
-        out2 = self.Conv3(feature)
-        feature = self.AvgPool(out2)
-        out1 = self.Conv4(feature)
-        return out1, out2
+        out_m = self.Conv3(feature)
+        feature = self.AvgPool(out_m)
+        out_s = self.Conv4(feature)
+        return out_s, out_m
