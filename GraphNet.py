2c7a15bc9dbececba94f3cbb1ae3c355c3d80f0c
Hongjie97 <944685606@qq.com>
2022-11-23 15:51:46 +0800

    Update GraphNet.py

--- GraphNet.py
+++ GraphNet.py
@@ -5,49 +5,49 @@ from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
 
 
 class Graph_Transformer(nn.Module):
-    def __init__(self, input_dim, heads, hidden_dim):
+    def __init__(self, input_dim, head_num, hidden_dim):
         super(Graph_Transformer, self).__init__()
-        #  Multi-head self-attention
-        self.graph_conv = TransformerConv(input_dim, input_dim // heads, heads)
+        #  multi-head self-attention
+        self.graph_conv = TransformerConv(input_dim, input_dim//head_num, head_num)
         self.lin_out = nn.Linear(input_dim, input_dim)
 
-        # Feed Forward Network
+        # feed forward network
         self.ln1 = nn.LayerNorm(input_dim)
         self.ln2 = nn.LayerNorm(input_dim)
         self.lin1 = nn.Linear(input_dim, hidden_dim)
         self.lin2 = nn.Linear(hidden_dim, input_dim)
         self.act = nn.ReLU()
 
-    def forward(self, x, edge):
-        #  Multi-head self-attention
-        output1 = self.lin_out(self.graph_conv(x, edge))
+    def forward(self, x, edge_index):
+        #  multi-head self-attention
+        out1 = self.lin_out(self.graph_conv(x, edge_index))
 
-        # Feed Forward Network
-        output2 = self.ln1(output1 + x)
-        output3 = self.lin2(self.act(self.lin1(output2)))
-        output4 = self.ln2(output3 + output2)
+        # feed forward network
+        out2 = self.ln1(out1 + x)
+        out3 = self.lin2(self.act(self.lin1(out2)))
+        out4 = self.ln2(out3 + out2)
 
-        return output4
+        return out4
 
 
 class GraphNet(nn.Module):
-    def __init__(self, input_dim):
+    def __init__(self, input_dim, head_num=4, hidden_dim=64, ratio=0.8):
         super(GraphNet, self).__init__()
-        self.conv1 = Graph_Transformer(input_dim, heads=4, hidden_dim=64)
-        self.pool1 = SAGPooling(input_dim, ratio=0.8)
-        self.conv2 = Graph_Transformer(input_dim, heads=4, hidden_dim=64)
-        self.pool2 = SAGPooling(input_dim, ratio=0.8)
-        self.conv3 = Graph_Transformer(input_dim, heads=4, hidden_dim=64)
-        self.pool3 = SAGPooling(input_dim, ratio=0.8)
-        self.lin1 = nn.Linear(input_dim * 2, input_dim)
-        self.lin2 = nn.Linear(input_dim, input_dim // 2)
-        self.lin3 = nn.Linear(input_dim // 2, 1)
+        self.conv1 = Graph_Transformer(input_dim, head_num, hidden_dim)
+        self.pool1 = SAGPooling(input_dim, ratio)
+        self.conv2 = Graph_Transformer(input_dim, head_num, hidden_dim)
+        self.pool2 = SAGPooling(input_dim, ratio)
+        self.conv3 = Graph_Transformer(input_dim, head_num, hidden_dim)
+        self.pool3 = SAGPooling(input_dim, ratio)
+        self.lin1 = nn.Linear(input_dim*2, input_dim)
+        self.lin2 = nn.Linear(input_dim, input_dim//2)
+        self.lin3 = nn.Linear(input_dim//2, 1)
         self.bn1 = nn.BatchNorm1d(input_dim)
-        self.bn2 = nn.BatchNorm1d(input_dim // 2)
+        self.bn2 = nn.BatchNorm1d(input_dim//2)
         self.act = nn.ReLU()
 
-    def forward(self, data):
-        x, edge_index, batch = data.x, data.edge_index, data.batch
+    def forward(self, graph_data):
+        x, edge_index, batch = graph_data.x, graph_data.edge_index, graph_data.batch
         x = self.conv1(x, edge_index)
 
         x, edge_index, _, batch, perm, score = self.pool1(x, edge_index, None, batch)
@@ -63,6 +63,7 @@ class GraphNet(nn.Module):
         x, edge_index, _, batch, perm, score = self.pool3(x, edge_index, None, batch)
         x3 = torch.cat([gmp(x, batch), gap(x, batch)], dim=1)
 
+        # multi-level features from read out layers
         x_feature = x1 + x2 + x3
 
         x_out = self.act(self.bn1(self.lin1(x_feature)))
